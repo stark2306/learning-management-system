0d23e9258b85fad18d2585ff0d5be95503707f38
wilson.kartik <wilson.kartik@rubicotech.in>
2022-12-19 16:24:58 +0530

    added client-side validation to check whether the username, email and contact being entered is unique or not to make sure only unique values for these fields are entered in db

--- includes/js/registration-form-validation.js
+++ includes/js/registration-form-validation.js
@@ -1,9 +1,86 @@
 $(document).ready(function () {
-
+    $.getJSON('../../db-queries/connect.php', function(data) {
+        console.log(data);
+   })
     function trim_data(field_data) {
         let data = field_data.trim();
         return data;
     }
+
+    function show_validation_message(selector_in_focus, exist_status){
+        if(exist_status)
+        {
+            $(selector_in_focus).after("<span class='not-available'>Not Available</span>");
+            $("button.registration_submit_btn").attr({"disabled":true})
+            $("button.registration_submit_btn").css("text-decoration","line-through");
+        }
+        else if(!exist_status)
+        {
+            $(selector_in_focus).after("<span class='available'>Available</span>");
+            $("button.registration_submit_btn").attr({"disabled":false})
+            $("button.registration_submit_btn").css("text-decoration","none");
+        }
+    }
+
+    function show_validations(data,selector_in_focus,selector,value_entered){
+        console.log(value_entered)
+        let comparison_value = "";
+        exist_status = false;
+        for(let i=0;i<data.length;i++)
+        {  
+            $('.not-available').remove(); 
+            $('.available').remove();
+            $('.short-username').remove();
+            if(selector == 'email'){
+                comparison_value = data[i].email;
+            }
+            else if(selector == 'contact' && value_entered.length == 10){
+                if (value_entered == data[i].contact)
+                {
+                    exist_status = true;
+                }
+                show_validation_message(selector_in_focus,exist_status)
+            }
+            else
+            { 
+                if(selector == 'username' && value_entered.length >= 5){
+                if (value_entered == data[i].username)
+                {
+                    exist_status = true;
+                }
+                show_validation_message(selector_in_focus,exist_status)
+            }
+            if(selector == 'username' && value_entered.length <4 && value_entered.length!=0){
+                $("input#username").after("<span class='short-username'>Please choose a username which has atleast length of 4</span>");
+            }   
+        }
+
+            if (value_entered == comparison_value)
+            {
+                exist_status = true;
+                break;
+            }
+        }
+    }
+
+    function check_validity(selector){
+        let selector_in_focus = "input#".concat(selector)
+      
+            $(selector_in_focus).on('keyup',function(){
+                let value_entered = $(selector_in_focus).val();
+                $.getJSON('../../db-queries/get-db-data.php', function(data) {
+                   setInterval(show_validations(data,selector_in_focus,selector,value_entered),5000)
+                })
+            });
+        }
+
+    $("input").focusin(function(){
+        let selector = $(this).attr('id');
+        if(selector == 'email' || selector == 'contact' || selector == 'username')
+        {
+            check_validity(selector);
+        }
+    });
 
     $('button.registration_submit_btn').on('click', () => {
         $("#registration_form>input").each(function (event) {
@@ -56,6 +133,7 @@ $(document).ready(function () {
         }
     });
 
+    
    function validation_message(selector_id, field_value) {
         if (!field_value) {
             selector_id = '#'.concat(selector_id);
