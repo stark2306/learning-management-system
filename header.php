5eeaced10fad932a5e40535065e7ff15e0630737
wilson.kartik <wilson.kartik@rubicotech.in>
2022-12-19 16:36:46 +0530

    header of the website

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cricket Academy</title>
+    <link rel="stylesheet" href="/includes/css/style.css">
+    <link rel="stylesheet" href="/includes/css/bootstrap.min.css">    
+    <script src="/includes/js/bootstrap.bundle.min.js"></script>      <!-- include bootstrap -->
+    <script src="/includes/js/jquery-3.6.2.min.js"></script>                 <!-- include jquery -->
+</head>
+<body>
+    
+<a href="/register.php">Sign Up</a>
+<a href="/login.php">Login</a>
