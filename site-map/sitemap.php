f320a1e571a0337a29e283e00a4a5c14ad8476c2
wilson.kartik <wilson.kartik@rubicotech.in>
2023-01-02 14:38:08 +0530

    files to generate the xml of the website

--- site-map/sitemap.php
+++ site-map/sitemap.php
@@ -1,13 +1,14 @@
 <?php
 //sitemap.php
 include '../db-queries/connect.php';
-
+isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? $current_url = "https://" : $current_url = "http://";
+$current_url.= $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];    ;
 $query = "SELECT uri FROM page";
 $result = mysqli_query($connection, $query);
-$base_url = "http://cricketacademy.test/site-map";
+$base_url = $current_url."/site-map";
 header("Content-Type: application/xml; charset=utf-8");
-echo '<?xml version="1.0" encoding="UTF-8"?>' .PHP_EOL;
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\
+echo '<!-- ?xml version="1.0" encoding="UTF-8"?-->' .PHP_EOL;
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://
 www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' .PHP_EOL;
@@ -19,6 +20,5 @@ echo '<changefreq>daily</changefreq>'.PHP_EOL;
 echo '</url>' . PHP_EOL;
 }
 echo '</urlset>'.PHP_EOL;
-
 mysqli_close($connection);
 ?>
