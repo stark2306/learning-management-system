587ff76ff82adeb7e9be89ee6fd74e79c261e20d
wilson.kartik <wilson.kartik@rubicotech.in>
2023-01-02 12:41:20 +0530

    updated main tags in the code'

--- class.php
+++ class.php
@@ -3,8 +3,14 @@
   //Template: class (memberships)
 
   include_once 'header.php';    //header of the website
+  ?>
+  <main>
 
-  include_once './includes/class_cards.php';
+  <?php
+    include_once './includes/class_cards.php';
+  ?>
 
+</main>
+<?php
   include_once 'footer.php';    //footer of the website
 ?>
